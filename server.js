540945bc9883129c0abba60e96504ee7e83895ca
Aryan <aryans6204@gmail.com>
2024-09-05 18:33:51 +0530

    third commit

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ import homeRouter from './router/home.router.js'
 env.config();
 
 const corsAllow = {
-    origin: 'http://localhost:5173',
+    origin: 'http://localhost:5173,https://harmonious-churros-344c44.netlify.app',
     method: 'POST, GET, PUT, PATCH, HEAD',
     credential: true
 }
